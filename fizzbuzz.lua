8fa44634b92d426fd8117aaa5c7a23ba47751040
okuzawats <okuzawats@gmail.com>
2024-01-13 06:09:06 +0900

    ループ処理をmain.luaに移動

--- fizzbuzz.lua
+++ fizzbuzz.lua
@@ -7,5 +7,3 @@ function fizzbuzz(n)
   if n % 5 < 1 then s = s.."Buzz" end
   return s ~= "" and s or tostring(n)
 end
-
-for i = 1, 100 do print(fizzbuzz(i)) end
---
+++ main.lua
@@ -0,0 +1,5 @@
+require "fizzbuzz"
+
+for i = 1, 100 do
+  print(fizzbuzz(i))
+end
