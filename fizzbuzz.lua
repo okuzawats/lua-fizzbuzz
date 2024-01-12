424f98514d37b27f682d7e0b6c1aa25a9cf690dc
okuzawats <okuzawats@gmail.com>
2024-01-12 13:57:38 +0900

    fizzbuzz関数にドキュメントコメントを追加

--- fizzbuzz.lua
+++ fizzbuzz.lua
@@ -1,3 +1,6 @@
+--- FizzBuzzを実行し、文字列を返す。
+-- @param n 対象となる整数
+-- @return nが3の倍数の場合はFizz、5の倍数の場合はBuzz、3と5の倍数の場合はFizzBuzz、それ以外の場合はnを文字列に変換したものを返す。
 function fizzbuzz(n)
   s = ""
   if n % 3 < 1 then s = s.."Fizz" end
